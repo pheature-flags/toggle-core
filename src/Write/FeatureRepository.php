909814d36f2545f765065ce2368cd966b21ebe73
xserrat <xavier.serrat22@gmail.com>
2021-10-06 23:37:38 +0200

    [#30] Update FeatureRepository::remove contract to accept Feature instead of FeatureId

--- src/Write/FeatureRepository.php
+++ src/Write/FeatureRepository.php
@@ -7,6 +7,6 @@ namespace Pheature\Core\Toggle\Write;
 interface FeatureRepository
 {
     public function save(Feature $feature): void;
-    public function remove(FeatureId $featureId): void;
+    public function remove(Feature $feature): void;
     public function get(FeatureId $featureId): Feature;
 }
